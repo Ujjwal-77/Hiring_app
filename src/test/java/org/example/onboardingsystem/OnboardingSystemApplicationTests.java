82a7e01a54d9b0eb8cd5cf17d7e72f03bc17aa8d
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-05-14 15:41:22 +0530

    [Ujjwal] Add: Basic test class for application context loading

---
+++ src/test/java/org/example/onboardingsystem/OnboardingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onboardingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
