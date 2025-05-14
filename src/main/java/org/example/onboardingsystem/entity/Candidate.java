673ee8617e86562bd976cd816114bef399d77206
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-05-14 15:41:18 +0530

    [Ujjwal] Add: Entity representing candidate details

---
+++ src/main/java/org/example/onboardingsystem/entity/Candidate.java
@@ -0,0 +1,17 @@
+package org.example.onboardingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Status status; // This should be fine as long as Status is an enum
+}
