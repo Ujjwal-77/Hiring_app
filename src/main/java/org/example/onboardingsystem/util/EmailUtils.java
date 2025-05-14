d3473f3f5a6e2b3f44c0ea00c9f1d4fdb0ab81ab
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-05-14 15:41:21 +0530

    [Ujjwal] Add: Additional email-related utility functions

---
+++ src/main/java/org/example/onboardingsystem/util/EmailUtils.java
@@ -0,0 +1,26 @@
+package org.example.onboardingsystem.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class EmailUtils {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, false);
+        emailSender.send(message);
+    }
+}
