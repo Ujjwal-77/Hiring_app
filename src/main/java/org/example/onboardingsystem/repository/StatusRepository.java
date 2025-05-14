903bfa6b3667cfb59f49e9c124d61022ecef61fc
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-05-14 15:41:20 +0530

    [Ujjwal] Add: JPA repository interface for Status entity

---
+++ src/main/java/org/example/onboardingsystem/repository/StatusRepository.java
@@ -0,0 +1,7 @@
+//package org.example.onboardingsystem.repository;
+//
+//import org.example.onboardingsystem.entity.Status;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface StatusRepository extends JpaRepository<Status, Long> {
+//}
