8fab91bf00dc87c7a38237807f8b31bc90e6f338
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-05-14 15:41:20 +0530

    [Ujjwal] Add: JPA repository interface for Candidate entity

---
+++ src/main/java/org/example/onboardingsystem/repository/CandidateRepository.java
@@ -0,0 +1,9 @@
+package org.example.onboardingsystem.repository;
+
+import org.example.onboardingsystem.entity.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+}
